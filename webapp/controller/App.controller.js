b3979f0f7283670773fa54289d20e6d78e14df92
Phanindra <Phanindrasidhu@gmail.com>
2025-05-03 16:05:50 +0530

    Odata service integrate

--- webapp/controller/App.controller.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define(["ey/sd/sls/controller/BaseController"],function(Controller){
-    return Controller.extend("ey.sd.sls.controller.App",{
-        
-    });
-});
--- webapp/controller/BaseController.js
+++
@@ -1,8 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(Controller){
-    return Controller.extend("ey.sd.sls.controller.BaseController",{
-        x: "anubhav",
-        thisIsAKillerFunction: function(){
-            alert("I am a killer function " + this.x);
-        }
-    });
-});
